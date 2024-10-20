039f5083d5d1da1549458eb5996380f0979b8b64
OhKeon <dhrjs0503@gmail.com>
2024-10-20 16:41:21 +0900

    빈 문자열,null을 처리하는 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,7 +10,7 @@ public class Application {
            }
 
            //1. 기본 구분자로 문자열을 분리해서 합산
-        public static int Sum(String input) {
+        public static int CalculateSum(String input) {
             String[] tokens = input.split(",|:"); //쉼표 또는 콜론으로 기본구분자 처리
             int sum = 0;
             for (String token : tokens){
@@ -18,7 +18,15 @@ public class Application {
             }
             return  sum;
     }
-    
+
+    // 2. 빈 문자열 , null을 처리후에 계산
+    public static  int Add (String input) {
+        if(input == null || input.isEmpty()){
+            return 0;
+        }
+        return  CalculateSum(input);
+    }
+
 
 
 }
