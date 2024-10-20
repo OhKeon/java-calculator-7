aafafabebb21bf3007efe2ecca58847adf16254c
OhKeon <dhrjs0503@gmail.com>
2024-10-20 16:37:58 +0900

    기본구분자 합산기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,25 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
 
            }
+
+           //1. 기본 구분자로 문자열을 분리해서 합산
+        public static int Sum(String input) {
+            String[] tokens = input.split(",|:"); //쉼표 또는 콜론으로 기본구분자 처리
+            int sum = 0;
+            for (String token : tokens){
+                sum += Integer.parseInt(token);
+            }
+            return  sum;
+    }
+    
+
+
 }
 
