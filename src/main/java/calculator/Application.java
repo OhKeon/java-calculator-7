cf4297d2510e08ef6ba9217fcdae1fc4932a4692
OhKeon <dhrjs0503@gmail.com>
2024-10-20 16:51:25 +0900

    커스텀 구분자로 나눠서 더하는 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,5 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -10,7 +8,7 @@ public class Application {
            }
 
            //1. 기본 구분자로 문자열을 분리해서 합산
-        public static int CalculateSum(String input) {
+        public static int calculateSum(String input) {
             String[] tokens = input.split(",|:"); //쉼표 또는 콜론으로 기본구분자 처리
             int sum = 0;
             for (String token : tokens){
@@ -20,14 +18,30 @@ public class Application {
     }
 
     // 2. 빈 문자열 , null을 처리후에 계산
-    public static  int Add (String input) {
+    public static  int add (String input) {
         if(input == null || input.isEmpty()){
             return 0;
         }
-        return  CalculateSum(input);
+        if (input.startsWith("//")) {
+            int delimiterIndex = input.indexOf("\n");
+            String delimiter = input.substring(2, delimiterIndex); // 커스텀 구분자 추출
+            String numbers = input.substring(delimiterIndex + 1); // 숫자 부분 추출
+            return calculateSum(numbers, delimiter);
+        }
+
+        return  calculateSum(input);
     }
 
+    //3. 커스텀 구분자 문자열 분리
+    public static int calculateSum(String input,String delimiter){
+        String[] tokens = input.split(delimiter);
+        int sum = 0;
 
+        for (String token : tokens){
+             sum += Integer.parseInt(token);
+            }
+            return  sum;
+        }
 
 }
 
