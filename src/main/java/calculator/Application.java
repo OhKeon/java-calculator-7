22264b69bdf73b50816569988fede4ec2456f54c
OhKeon <dhrjs0503@gmail.com>
2024-10-20 17:08:10 +0900

    음수 입력시 예외처리하는 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,6 +10,7 @@ public class Application {
            //1. 기본 구분자로 문자열을 분리해서 합산
         public static int calculateSum(String input) {
             String[] tokens = input.split(",|:"); //쉼표 또는 콜론으로 기본구분자 처리
+            validateTokens(tokens); //음수인지 먼저 검증
             int sum = 0;
             for (String token : tokens){
                 sum += Integer.parseInt(token);
@@ -35,6 +36,7 @@ public class Application {
     //3. 커스텀 구분자 문자열 분리
     public static int calculateSum(String input,String delimiter){
         String[] tokens = input.split(delimiter);
+        validateTokens(tokens); //음수인지 먼저 검증
         int sum = 0;
 
         for (String token : tokens){
@@ -43,5 +45,14 @@ public class Application {
             return  sum;
         }
 
+   //4. 음수 입력시 예외 처리
+   private static void validateTokens(String[] tokens) {
+       for (String token : tokens) {
+           int number = Integer.parseInt(token);
+           if (number < 0) {
+               throw new IllegalArgumentException("음수는 입력할 수 없습니다: " + number);
+           }
+       }
+   }
 }
 
